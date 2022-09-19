c1eefe1734c70cca0d7fd13bd6d215de885fc0f3
‘AMAYA <aamaya547@gmail.com>
2022-09-19 16:04:30 -0700

    title case (not add punctuation as stated above) is passing as expected

--- day-01/index.js
+++ day-01/index.js
@@ -11,4 +11,11 @@ function addPunctuation(string) {
   };
 }
 
-module.exports = { reverseSentence, addPunctuation };
+function titleCaseWords(string) {
+  return string
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+module.exports = { reverseSentence, addPunctuation, titleCaseWords };
